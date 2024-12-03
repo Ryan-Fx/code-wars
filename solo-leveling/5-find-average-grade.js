b137328d703ba1549d831757c5a233589ec7db10
Ryan-Fx <rinnd.dn@gmail.com>
2024-12-03 16:49:18 +0800

    avg grade

---
+++ solo-leveling/5-find-average-grade.js
@@ -0,0 +1,11 @@
+function getGrade(...s) {
+  const score = s.reduce((a, b) => a + b) / s.length;
+
+  if (score >= 90) return "A";
+  else if (score >= 80) return "B";
+  else if (score >= 70) return "C";
+  else if (score >= 60) return "D";
+  else return "F";
+}
+
+console.log(getGrade(60, 82, 76));
